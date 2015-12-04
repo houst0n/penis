280343d18664aae0a42269adaf857ff1d5611e12
Neil Houston <neil.a.houston@gmail.com>
2015-12-04 15:06:56 +0000

    Scottish Mode: Experimental

--- putpenis.c
+++ putpenis.c
@@ -9,22 +9,30 @@ void putpenis(char *str)
 }
 int main(int argc, char **argv)
 {
+   printf("++++ SCOTTISH MODE ++++\n");
    printf("watch %s's ass get owned by %s!!\n",(argc>1)?argv[1]:"matt",
           (argc>2)?argv[2]:"houst0n");
 
   while(1)
   {
-    putpenis("8====D (_O_)");
-    putpenis(" 8====D(_O_)");
-    putpenis("  8====D_O_)");
-    putpenis("   8====DO_)");
-    putpenis("    8====D_)");
-    putpenis("     8====_)");
-    putpenis("      8===_)");
-    putpenis("     8====_)");
-    putpenis("    8====D_)");
-    putpenis("  8====D_O_)");
-    putpenis(" 8====D(_O_)");
+    putpenis("8=====D  (_O_)");
+    putpenis(" 8=====D (_O_)");
+    putpenis("  8=====D(_O_)");
+    putpenis("   8=====D_O_)");
+    putpenis("    8=====DO_)");
+    putpenis("     8=====D_)");
+    putpenis("      8=====_)");
+    putpenis("       8====_)");
+    putpenis("        8===_)");
+    putpenis("         8==_)");
+    putpenis("       8====_)");
+    putpenis("      8=====_)");
+    putpenis("     8=====D_)");
+    putpenis("    8=====DO_)");
+    putpenis("   8=====D_O_)");
+    putpenis("  8=====D(_O_)");
+    putpenis(" 8=====D (_O_)");
+    putpenis("8=====D  (_O_)");
   }
   return 0;
 }
